fd630ad6b2e0e24db61a8c53f55be23a44df9bd3
daavsnts <daavsantosl@gmail.com>
2025-06-19 05:41:58 -0300

    fix: barbar error when leaving nvim

--- lua/daavsnts/set.lua
+++ lua/daavsnts/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ''
+vim.opt.guicursor = ""
 
 vim.opt.nu = true
 --vim.opt.relativenumber = true
@@ -14,7 +14,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = vim.fn.stdpath('config') .. '/undo'
+vim.opt.undodir = vim.fn.stdpath("config") .. "/undo"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -22,31 +22,40 @@ vim.opt.hlsearch = false
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = 'yes'
-vim.opt.isfname:append('@-@')
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 --vim.opt.timeoutlen = 2000
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.podspec", "Podfile" },
-  command = "set filetype=ruby",
+	pattern = { "*.podspec", "Podfile" },
+	command = "set filetype=ruby",
 })
 
 vim.opt.laststatus = 3
 
 function DisableBackground()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
-  vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
-  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-  --vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 end
 
-vim.api.nvim_create_user_command('Dbg', DisableBackground, {})
+vim.api.nvim_create_user_command("Dbg", DisableBackground, {})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+	callback = function()
+		local listed_buffers = vim.fn.getbufinfo({ buflisted = 1 })
+		if #listed_buffers == 0 then
+			vim.cmd("enew")
+		end
+	end,
+})
